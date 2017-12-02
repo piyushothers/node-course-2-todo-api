a09a9076b90106aef562145d3fd39ac317ef7fd6
Piyush Tripathi <piyushothers@gmail.com>
2017-12-02 20:17:05 +0530

    add post routes

---
+++ server/server.js
@@ -0,0 +1,28 @@
+
+var {mongoose} = require('./db/mongoose.js')
+var {Todo} = require('./models/todo.js')
+var {User} = require('./models/users.js')
+
+
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(bodyParser.json())
+app.post('/todos',(req,res)=>{
+    console.log(req.body)
+    var todo = new Todo({
+        text : req.body.text
+    }) ; 
+
+    todo.save().then((doc)=>{
+        res.send(doc)
+    },(err)=>{
+        res.status(400).send(err)
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('started server on port 3000')
+})
